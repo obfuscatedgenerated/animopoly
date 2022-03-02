91e066a0416b7e059e63447fef91241addccaacd
obfuscatedgenerated <75035364+obfuscatedgenerated@users.noreply.github.com>
2022-03-02 11:12:06 +0000

    dice methods

--- src/main/java/ml/obfuscatedgenerated/Animopoly/Board.java
+++ src/main/java/ml/obfuscatedgenerated/Animopoly/Board.java
@@ -1,4 +1,23 @@
-package main.Animopoly;
+package ml.obfuscatedgenerated.Animopoly;
+
+import java.util.Random;
+import java.util.ArrayList;
 
 public class Board {
+    private static final Random rng = new Random();;
+
+    public static ArrayList<Integer> dice() {
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        al.add(rng.nextInt(6)+1);
+        al.add(rng.nextInt(6)+1);
+        return al;
+    }
+
+    public static boolean areDoubles(ArrayList<Integer> al) {
+        return (al.get(0) == al.get(1));
+    }
+
+    public static int sumDice(ArrayList<Integer> al) {
+        return al.get(0) + al.get(1);
+    }
 }
---
+++ src/main/java/ml/obfuscatedgenerated/Animopoly/Main.java
@@ -0,0 +1,7 @@
+package ml.obfuscatedgenerated.Animopoly;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Board.areDoubles(Board.dice()));
+    }
+}
